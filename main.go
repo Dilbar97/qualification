aac6e0991413aa15144dc38ad1aad5078f691209
ekaterina.babanova@tages.ru <ekaterina.babanova@tages.ru>
2023-10-18 09:56:56 +0500

    2

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import (
+	"qualification/cmd"
+)
+
+func main() {
+	cmd.Execute()
+
+	//_ = http.ListenAndServe(":3333", nil)
+}
